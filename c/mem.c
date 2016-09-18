1f70d10ba57c413f177687d0491f8486ee9fde5f
Teng Liu <x8b9@lin10.ugrad.cs.ubc.ca>
2016-09-17 18:22:43 -0700

    gives away address of free mem in kmalloc

--- c/mem.c
+++ c/mem.c
@@ -25,17 +25,44 @@ void kmeminit(void){
   //set free memory linked list
   memSlot = (struct memHeader *)freemem;
   memSlot->size = HOLESTART-freemem;
-  memSlot->next = HOLEEND;
-  memSlot->sanityCheck = 125;
+  memSlot->next = (struct memHeader *)HOLEEND;
+  memSlot->sanityCheck = "pizzahut";
 
-  memSlot->next->prev = memSlot;
+  memSlot->next->prev = (struct memHeader *)memSlot;
   memSlot->next->next = NULL;
-  memSlot->next->size = maxaddr - HOLEEND;
-  memSlot->next->sanityCheck = 125;
+  memSlot->next->size = (unsigned long)(maxaddr - HOLEEND);
+  memSlot->next->sanityCheck = "pizzahut";
 
 
   kprintf("maxaddr: %d, HOLEEND: %d, memSlot->next->size: %d, HOLEEND: %d\n", maxaddr, HOLEEND, memSlot->next->size, HOLEEND);
   
+
+}
+
+//give set amount of free mem
+void *kmalloc(int size){
+	//check size if available in memSlot
+	struct memHeader *m = memSlot;
+	for(m; m->size < size; m = m->next){
+		//move pointer if mem less than size
+	}
+	if(m == NULL){
+		return -1; //error, not enough RAM
+	}
+
+	struct memHeader *returnNode = m;
+
+	//take node out of list
+	if(m->prev != NULL){
+		m->prev->next = m->next;
+	}
+	if(m->next != NULL){
+		m->next->prev = m->prev;
+	}
+
+	//give RAM
+	return &returnNode;
+
 
 }
 
