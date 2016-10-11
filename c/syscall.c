e94fad99a35c139c7ded07e92fbad4cbb25869d1
wazloes <wazloes@dhcp-206-87-112-102.ubcsecure.wireless.ubc.ca>
2016-10-11 11:10:27 -0700

    syscall

--- c/syscall.c
+++ c/syscall.c
@@ -4,3 +4,36 @@
 #include <xeroskernel.h>
 
 /* Your code goes here */
+
+
+unsigned int syscall(num, inst, ...){
+	int result;
+
+	//TODO: put arguments in register
+	__asm __volatile("\
+
+
+			movl %eax, inst
+			int 49
+			movl __result, %eax
+		"
+		:
+		:
+		: "%eax"
+		);
+
+	return result
+}
+
+
+extern unsigned int syscreate( void (*func)(void), int stack ){
+	return syscall(3, "CREATE", *func, stack);
+}
+
+
+extern void sysyield( void ){
+	return syscall(1, "YEILD");
+}
+extern void sysstop( void ){
+	return syscall(1, "STOP");
+}
